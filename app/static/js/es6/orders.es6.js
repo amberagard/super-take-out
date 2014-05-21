7c8f9425e98e69385459d9f32fa48d1fa4663870
Amber Agard <amber.n.agard@gmail.com>
2014-05-20 21:09:25 -0500

    stuff

---
+++ app/static/js/es6/orders.es6.js
@@ -0,0 +1,33 @@
+/* global ajax */
+/* jshint unused:false */
+
+(function() {
+    'use strict';
+
+    $(document).ready(initialize);
+
+    function initialize() {
+        $('.menu').change(getMenu);
+        $('#add').click(add);
+    }
+
+    function getMenu() {
+        var menu = $(this).val();
+        var next = $(this).next();
+        ajax(`/dishes/${menu}`, 'get', null, h=>{
+            next.empty().append(h);
+        });
+    }
+
+    function add(e) {
+        var quantity = $('#input').val();
+        var menu = $('.menu').val();
+        var dish = $('.dish').val();
+        console.log(quantity);
+        console.log(menu);
+        console.log(dish);
+        e.preventDefault();
+    }
+
+
+})();
